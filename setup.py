63471448b01e8c49c795b099755f15e58c5dbc6f
仙 <110833020+tianhuzong@users.noreply.github.com>
2024-06-23 22:43:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extensions = [
 setup(
     name="ThzHashMap",
     ext_modules=cythonize(extensions),
-    version="1.0.1",               # 版本号
+    version="1.0.2",               # 版本号
     author="Sen",
     package=['thzhashmap'],
     package_data={"thzhashmap": ["thzhashmap/*.pyx"]},
